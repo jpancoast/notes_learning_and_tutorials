093094c119719db7fa12c676e5ad7e4b76b11e21
James Pancoast <jpancoast@petabloc.com>
2023-05-23 16:44:26 -0600

    Trying out some transit gateways

---
+++ terraform/aws/transit_gateway/provider.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-west-2"
+  alias  = "us-west-2"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias  = "us-east-2"
+}
---
+++ terraform/aws/transit_gateway/vpc.tf
@@ -0,0 +1,87 @@
+#
+#   us-west-2 VPC's
+#
+
+#tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
+module "us-west-2-vpc-1" {
+  providers = {
+    aws = aws.us-west-2
+  }
+
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
+
+  name = "us-west-2-vpc-1"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a"]
+  private_subnets = ["10.0.1.0/24"]
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
+#tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
+module "us-west-2-vpc-2" {
+  providers = {
+    aws = aws.us-west-2
+  }
+
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
+
+  name = "us-west-2-vpc-2"
+  cidr = "10.1.0.0/16"
+
+  azs             = ["us-west-2a"]
+  private_subnets = ["10.1.1.0/24"]
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
+#
+#   us-east-2 VPC's
+#
+
+#tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
+module "us-east-2-vpc-1" {
+  providers = {
+    aws = aws.us-east-2
+  }
+
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
+
+  name = "us-east-2-vpc-1"
+  cidr = "10.3.0.0/16"
+
+  azs             = ["us-east-2a"]
+  private_subnets = ["10.3.1.0/24"]
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
+#tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
+module "us-east-2-vpc-2" {
+  providers = {
+    aws = aws.us-east-2
+  }
+
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
+
+  name = "us-east-2-vpc-2"
+  cidr = "10.4.0.0/16"
+
+  azs             = ["us-east-2a"]
+  private_subnets = ["10.4.1.0/24"]
+
+  tags = {
+    Terraform = "true"
+  }
+}
